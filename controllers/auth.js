13d6c2dad23567a8c1400fe8826038f448a59d95
AndresKogan <61512404+AndresKogan@users.noreply.github.com>
2021-11-02 18:56:18 -0300

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs')
 
 const { generarJWT } = require('../helpers/generar-jwt')
 const Usuario = require('../models/usuario');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
@@ -50,7 +51,56 @@ const login = async (req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: ' ',
+                img,
+                google: true
+            }
+
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+        }
+
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+
+        })
+    } catch (error) {
+
+        res.status(400).json({
+            msg: 'Token google no es valido'
+        })
+
+    }
+
+
+
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
